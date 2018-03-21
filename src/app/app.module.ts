ce0a16df4e976d5bcffb3be3c286c971b4417ed2
Jose Tapia <jose.tapia@ki.se>
2018-03-21 12:08:34 +0100

    First version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    LocalNotifications,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,103 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  notifyTime: any;
+  notifications: any[] = [];
+  days: any[];
+  chosenHours: number;
+  chosenMinutes: number;
+
+  constructor(public navCtrl: NavController,
+    public platform: Platform,
+    public alertCtrl: AlertController,
+    public localNotifications: LocalNotifications) {
+
+      this.notifyTime = moment(new Date()).format();
+      this.chosenHours = new Date().getHours();
+      this.chosenMinutes = new Date().getMinutes();
+
+      this.days = [
+        {title: 'Monday', dayCode: 1, checked: false},
+        {title: 'Tuesday', dayCode: 2, checked: false},
+        {title: 'Wednesday', dayCode: 3, checked: false},
+        {title: 'Thursday', dayCode: 4, checked: false},
+        {title: 'Friday', dayCode: 5, checked: false},
+        {title: 'Saturday', dayCode: 6, checked: false},
+        {title: 'Sunday', dayCode: 0, checked: false}
+    ];
+
+  }
+
+  ionViewDidLoad(){
+
+  }
+
+  timeChange(time){
+    this.chosenHours = time.hour.value;
+    this.chosenMinutes = time.minute.value;
+  }
+
+  addNotifications() {
+    let currentDate = new Date();
+    let currentDay = currentDate.getDay(); // Sunday = 0, Monday = 1, and so on.
+
+    for(let day of this.days) {
+
+      if(day.checked) {
+
+        let firstNotificationsTime = new Date();
+        let dayDifference = day.dayCode - currentDay;
+
+        if(dayDifference < 0) {
+          dayDifference = dayDifference + 7; // for cases where the day is in the following week
+        }
+
+        firstNotificationsTime.setHours(firstNotificationsTime.getHours() + (24 * (dayDifference)));
+        firstNotificationsTime.setHours(this.chosenHours);
+        firstNotificationsTime.setMinutes(this.chosenMinutes);
+
+        let notification = {
+          id: day.dayCode,
+          title: 'Hey!',
+          text: 'You just got notified :)',
+          at: firstNotificationsTime,
+          every: 'week'
+        }
+
+        this.notifications.push(notification);
+      }
+    }
+
+    console.log("Notifications to be scheduled: ", this.notifications);
+
+    if (this.platform.is('cordova')) {
+      // Cancell any existing notifications
+      this.localNotifications.cancelAll().then(() => {
+        //Schedule the new notifications
+        this.localNotifications.schedule(this.notifications);
+
+        this.notifications = [];
+
+        let alert = this.alertCtrl.create({
+          title: 'Notification set',
+          buttons: ['OK']
+        });
+
+        alert.present();
+      });
+    }
+  }
+
+  cancelAll(){
+
+    this.localNotifications.cancelAll();
+
+    let alert = this.alertCtrl.create({
+      title: 'Notifications cancelled',
+      buttons: ['OK']
+    });
+
+    alert.present();
 
   }
 
